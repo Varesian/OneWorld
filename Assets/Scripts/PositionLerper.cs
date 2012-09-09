df18e913b559914221a826d6d21a134354919b2b
Cory Barr <cory.barr@gmail.com>
2012-09-09 16:18:36 -0700

    eclipse done, no Z fighting

--- Assets/Scripts/PositionLerper.cs
+++ Assets/Scripts/PositionLerper.cs
@@ -5,13 +5,14 @@ public class PositionLerper : MonoBehaviour {
 	
 	public Vector3 endingPosition;
 	public float movementSpeed = 0.05f;
+	public string key = "A";
 	private Vector3 startPosition;
 	private float lerpAmount = 0;
 	private bool update = false;
 	
 	// Use this for initialization
 	void Start () {
-		startPosition = transform.position;
+		startPosition = transform.localPosition;
 	}
 	
 	// Update is called once per frame
@@ -19,13 +20,13 @@ public class PositionLerper : MonoBehaviour {
 		if (update) {
 			if (lerpAmount < 1.0f) {
 				lerpAmount += movementSpeed * Time.deltaTime;
-				transform.position = Vector3.Lerp(startPosition, endingPosition, lerpAmount);
+				transform.localPosition = Vector3.Lerp(startPosition, endingPosition, lerpAmount);
 			}
 		}
 	}
 	
 	void OnGUI() {
-		if (Event.current.Equals(Event.KeyboardEvent("A"))) {
+		if (Event.current.Equals(Event.KeyboardEvent(key))) {
 			update = !update;
 		}
 	}
